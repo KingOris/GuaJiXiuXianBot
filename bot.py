208ccb475c24bd68e3c98c3d62ec81ac8be18eaf
KingOris <47605407+KingOris@users.noreply.github.com>
2019-03-30 02:11:59 -0500

    Add more role add delete function add sell item function

--- bot.py
+++ bot.py
@@ -1,8 +1,10 @@
+import asyncio
 import discord
 from discord.ext import commands
 
 # Set the bot prefix
-Bot_Prefix = ('XX')
+Bot_Prefix = "xx!"
+
 # Set the bot token
 Token = 'NTU4NDU0MDYwMDAxMzk0Njkx.D3XH7w.fsLJMqlZkznsE_CU0__r6_BxIjs'
 
@@ -30,6 +32,7 @@ async def on_message(message):
     print(time)
     print(author)
     print(content)
+    await client.process_commands(message)
 
 
 # When new member join, automatically give a role and send message
@@ -38,8 +41,52 @@ async def on_member_join(member):
     role = discord.utils.get(member.server.roles, name='無知之徒')
     await client.add_roles(member, role)
     channel = discord.utils.get(member.server.channels, name='welcome')
-    await client.send_message(channel, '{}/{}.'.format('Role has been set up', '已添加身份'))
+    await client.send_message(channel, '{}/{}.'.format('Role has been set up', '已添加身份-無知之徒'))
 
+
+# For register the item for selling in market
+@client.command(pass_context=True)
+async def sell(ctx):
+    channel = ctx.message.channel
+    channel_name = channel.name
+
+    if channel_name != '黑市':
+        await client.say('黑市之外無法注冊')
+        await delete_message(channel, 2, 3)
+
+    else:
+        author = ctx.message.author
+        await client.send_message(author, '注冊成功')
+        await client.delete_message(ctx.message)
+
+
+# To add role for players
+@client.command(pass_context=True)
+async def begin(ctx):
+    member = ctx.message.author
+    role = discord.utils.get(member.server.roles, name='江湖之人')
+    await client.add_roles(member,role)
+    await client.say('開始你的修仙之路吧')
+    await delete_message(ctx.message.channel, 2, 3)
+
+
+# To add role for players
+@client.command(pass_context=True)
+async def chigua(ctx):
+    member = ctx.message.author
+    role = discord.utils.get(member.server.roles, name='吃瓜群衆')
+    await client.add_roles(member, role)
+    await client.say('去吃你的瓜吧')
+    await delete_message(ctx.message.channel, 2, 3)
+
+
+# To delete messages
+async def delete_message(channel, limit, time):
+    messages = []
+    async for message in client.logs_from(channel, limit):
+        messages.append(message)
+    await asyncio.sleep(time)
+    await client.delete_messages(messages)
 
 # Run bot with Token
 client.run(Token)
